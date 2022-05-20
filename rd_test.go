bf5faa0c8b59047a6e0832288965c60d7b6b0fa3
Yahya <chahineyahya@pm.me>
2022-05-20 18:20:41 +0100

    Added test cases for deleting a book

--- rd_test.go
+++ rd_test.go
@@ -16,7 +16,7 @@ func TestAddBook(t *testing.T) {
 	b.AddBook(item)
 
 	if b[0].Title != item.Title {
-		t.Errorf("Expected %q but got %q", b[0].Title, item.Title)
+		t.Errorf("expected %q but got %q", b[0].Title, item.Title)
 	}
 }
 
@@ -30,16 +30,52 @@ func TestMarkAsFinished(t *testing.T) {
 	b.AddBook(item)
 
 	if b[0].Title != item.Title {
-		t.Errorf("Expected %q but got %q", b[0].Title, item.Title)
+		t.Errorf("expected %q but got %q", b[0].Title, item.Title)
 	}
 
 	if b[0].Read {
-		t.Errorf("Newly added books should be marked as not finished")
+		t.Errorf("newly added books should be marked as not finished")
 	}
 
 	b[0].Read = true
 
 	if !b[0].Read {
 		t.Errorf("%q should be marked as finished", b[0].Title)
+	}
+}
+
+func TestDeleteBook(t *testing.T) {
+	b := rd.Bibliography{}
+	items := []rd.Book{
+		{
+			Title:  "The Go Programming Language",
+			Author: "Brian Kernighan, Alan Donovan",
+		},
+		{
+			Title:  "Advanced Algorithms and Data Structures",
+			Author: "Marcello La Rocca",
+		},
+		{
+			Title:  "Ultimate Go Notebook",
+			Author: "William Kennedy",
+		},
+	}
+
+	for _, v := range items {
+		b.AddBook(v)
+	}
+
+	if b[0].Title != items[0].Title {
+		t.Errorf("expected %q, but got %q", items[0].Title, b[0].Title)
+	}
+
+	b.DeleteBook(1)
+
+	if len(b) != 2 {
+		t.Errorf("expected the length of the bibliography to be %d, instead got %d", 2, len(b))
+	}
+
+	if b[0].Title != items[1].Title {
+		t.Errorf("expected %q, but got %q", items[0].Title, b[0].Title)
 	}
 }
